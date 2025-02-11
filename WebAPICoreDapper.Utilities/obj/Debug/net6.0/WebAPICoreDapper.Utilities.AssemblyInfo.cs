218ca61a59e9fd353fd74f363332707f25702c9c
nghgphuo <phuocnguyen36398@gmail.com>
2025-02-11 15:55:41 +0700

    Refactoring code

--- WebAPICoreDapper.Utilities/obj/Debug/net6.0/WebAPICoreDapper.Utilities.AssemblyInfo.cs
+++ WebAPICoreDapper.Utilities/obj/Debug/net6.0/WebAPICoreDapper.Utilities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPICoreDapper.Utilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65fd6bcca5db21673d84c13d4751897ffda6071")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPICoreDapper.Utilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPICoreDapper.Utilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
